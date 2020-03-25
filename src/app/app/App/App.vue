819df0837a86ead9409ada4a29a1365cfa883a47
Administrator <254556008@qq.com>
2020-03-26 00:48:01 +0800

    PC端适配开发完成 移动端布局90%

--- src/app/app/App/App.vue
+++ src/app/app/App/App.vue
@@ -1,240 +1,240 @@
 <template>
-  <div id="app" :class="$style.app">
-    <vue-notification-stack />
+    <div id="app" :class="$style.app">
+        <vue-notification-stack/>
 
-    <vue-navigation-progress :is-navigating="isNavigating" />
-    <!--顶部导航-->
-    <puddor-nav-bar />
-    <!--<vue-nav-bar/>-->
-    <router-view :class="$style.content" />
-    <!--<vue-footer />-->
-    <vue-cookie-consent
-      current-version="1.0.0"
-      :cookie-consent-version="cookieConsentVersion"
-      :set-cookie-consent-version="setCookieConsentVersion"
-    >
-      This is a cookie consent component which shows the cookie consent every time you change the version of the
-      consent.
-    </vue-cookie-consent>
+        <vue-navigation-progress class="navigation-progress-style" :is-navigating="isNavigating"/>
+        <!--顶部导航-->
+        <puddor-nav-bar/>
+        <!--<vue-nav-bar/>-->
+        <router-view :class="$style.content"/>
+        <!--<vue-footer />-->
+        <vue-cookie-consent
+                current-version="1.0.0"
+                :cookie-consent-version="cookieConsentVersion"
+                :set-cookie-consent-version="setCookieConsentVersion"
+        >
+            This is a cookie consent component which shows the cookie consent every time you change the version of the
+            consent.
+        </vue-cookie-consent>
 
-    <vue-sidebar v-show="false">
-      <vue-sidebar-group title="导航">
-        <vue-sidebar-group-item to="/">
-          <vue-icon-code />
-          一键找乐手
-        </vue-sidebar-group-item>
-        <vue-sidebar-group-item to="/">
-          <VueIconPuzzlePiece />
-          一键他乡宿
-        </vue-sidebar-group-item>
-        <vue-sidebar-group-item to="/">
-          <VueIconHashtag />
-          一键接商演
-        </vue-sidebar-group-item>
-        <vue-sidebar-group-item to="/example/counter">
-          <VueIconBook />
-          帮助中心
-        </vue-sidebar-group-item>
+        <vue-sidebar v-show="false">
+            <vue-sidebar-group title="导航">
+                <vue-sidebar-group-item to="/">
+                    <vue-icon-code/>
+                    一键找乐手
+                </vue-sidebar-group-item>
+                <vue-sidebar-group-item to="/">
+                    <VueIconPuzzlePiece/>
+                    一键他乡宿
+                </vue-sidebar-group-item>
+                <vue-sidebar-group-item to="/">
+                    <VueIconHashtag/>
+                    一键接商演
+                </vue-sidebar-group-item>
+                <vue-sidebar-group-item to="/example/counter">
+                    <VueIconBook/>
+                    帮助中心
+                </vue-sidebar-group-item>
 
-        <vue-sidebar-group-item>
-          <vue-select name="lang" id="lang" :options="languages" @input="localeSwitch" :value="getLocale" />
-        </vue-sidebar-group-item>
-      </vue-sidebar-group>
+                <vue-sidebar-group-item>
+                    <vue-select name="lang" id="lang" :options="languages" @input="localeSwitch" :value="getLocale"/>
+                </vue-sidebar-group-item>
+            </vue-sidebar-group>
 
-      <vue-sidebar-group title="Community">
-        <vue-sidebar-group-item :to="{ name: 'dashboard' }">
-          <vue-icon-hashtag />
-          DashBoard
-        </vue-sidebar-group-item>
+            <vue-sidebar-group title="Community">
+                <vue-sidebar-group-item :to="{ name: 'dashboard' }">
+                    <vue-icon-hashtag/>
+                    DashBoard
+                </vue-sidebar-group-item>
 
-        <vue-sidebar-group-item :to="{ name: 'counter' }">
-          <vue-icon-hashtag />
-          VueX Example
-        </vue-sidebar-group-item>
+                <vue-sidebar-group-item :to="{ name: 'counter' }">
+                    <vue-icon-hashtag/>
+                    VueX Example
+                </vue-sidebar-group-item>
 
-        <vue-sidebar-group-item :to="{ name: 'form' }">
-          <vue-icon-hashtag />
-          Form Example
-        </vue-sidebar-group-item>
-      </vue-sidebar-group>
+                <vue-sidebar-group-item :to="{ name: 'form' }">
+                    <vue-icon-hashtag/>
+                    Form Example
+                </vue-sidebar-group-item>
+            </vue-sidebar-group>
 
-      <vue-sidebar-group title="Documentation">
-        <vue-sidebar-group-item>
-          <a href="https://vuesion.github.io/docs/en/">
-            <vue-icon-book />
-            Documentation
-          </a>
-        </vue-sidebar-group-item>
+            <vue-sidebar-group title="Documentation">
+                <vue-sidebar-group-item>
+                    <a href="https://vuesion.github.io/docs/en/">
+                        <vue-icon-book/>
+                        Documentation
+                    </a>
+                </vue-sidebar-group-item>
 
-        <vue-sidebar-group-item>
-          <a href="/storybook/?path=/story/design-system-design-system--intro">
-            <vue-icon-book />
-            Design System
-          </a>
-        </vue-sidebar-group-item>
+                <vue-sidebar-group-item>
+                    <a href="/storybook/?path=/story/design-system-design-system--intro">
+                        <vue-icon-book/>
+                        Design System
+                    </a>
+                </vue-sidebar-group-item>
 
-        <vue-sidebar-group-item>
-          <a href="/storybook/?path=/story/atoms-badge--badge-variants">
-            <vue-icon-puzzle-piece />
-            Components
-          </a>
-        </vue-sidebar-group-item>
-      </vue-sidebar-group>
-    </vue-sidebar>
+                <vue-sidebar-group-item>
+                    <a href="/storybook/?path=/story/atoms-badge--badge-variants">
+                        <vue-icon-puzzle-piece/>
+                        Components
+                    </a>
+                </vue-sidebar-group-item>
+            </vue-sidebar-group>
+        </vue-sidebar>
 
-    <vue-modal :show="showLoginModal" @close="showLoginModal = false">
-      <login-form :loading="isLoginPending" @submit="onLoginSubmit" />
-    </vue-modal>
-  </div>
+        <vue-modal :show="showLoginModal" @close="showLoginModal = false">
+            <login-form :loading="isLoginPending" @submit="onLoginSubmit"/>
+        </vue-modal>
+    </div>
 </template>
 
 <script lang="ts">
-import { mapActions, mapGetters } from 'vuex';
-import { loadLocaleAsync } from '@shared/plugins/i18n/i18n';
-import '@shared/designSystem/global.scss';
-import VueNavBar from '@components/VueNavBar/VueNavBar.vue';
-import PuddorNavBar from './componentsPuddor/puddorNavBar';
-import VueGrid from '@components/VueGrid/VueGrid.vue';
-import VueGridItem from '@components/VueGridItem/VueGridItem.vue';
-import VueFooter from '@components/VueFooter/VueFooter.vue';
-import VueNotificationStack from '@components/VueNotificationStack/VueNotificationStack.vue';
-import VueCookieConsent from '@components/VueCookieConsent/VueCookieConsent.vue';
-import VueNavigationProgress from '@components/VueNavigationProgress/VueNavigationProgress.vue';
-import VueSidebar from '@components/VueSidebar/VueSidebar.vue';
-import VueSidebarGroup from '@components/VueSidebar/VueSidebarGroup/VueSidebarGroup.vue';
-import VueSidebarGroupItem from '@components/VueSidebar/VueSidebarGroupItem/VueSidebarGroupItem.vue';
-import VueIconCode from '@components/icons/VueIconCode/VueIconCode.vue';
-import VueIconBook from '@components/icons/VueIconBook/VueIconBook.vue';
-import VueIconHashtag from '@components/icons/VueIconHashtag/VueIconHashtag.vue';
-import VueIconGithub from '@components/icons/VueIconGithub/VueIconGithub.vue';
-import VueIconTwitterSquare from '@components/icons/VueIconTwitterSquare/VueIconTwitterSquare.vue';
-import VueSelect from '@components/VueSelect/VueSelect.vue';
-import VueIconPuzzlePiece from '@components/icons/VueIconPuzzlePiece/VueIconPuzzlePiece.vue';
-import VueButton from '@components/VueButton/VueButton.vue';
-import VueModal from '@components/VueModal/VueModal.vue';
-import LoginForm from '@shared/modules/auth/LoginForm/LoginForm.vue';
-import { addNotification } from '@components/VueNotificationStack/utils';
+  import { mapActions, mapGetters } from 'vuex';
+  import { loadLocaleAsync } from '@shared/plugins/i18n/i18n';
+  import '@shared/designSystem/global.scss';
+  import VueNavBar from '@components/VueNavBar/VueNavBar.vue';
+  import PuddorNavBar from './componentsPuddor/puddorNavBar';
+  import VueGrid from '@components/VueGrid/VueGrid.vue';
+  import VueGridItem from '@components/VueGridItem/VueGridItem.vue';
+  import VueFooter from '@components/VueFooter/VueFooter.vue';
+  import VueNotificationStack from '@components/VueNotificationStack/VueNotificationStack.vue';
+  import VueCookieConsent from '@components/VueCookieConsent/VueCookieConsent.vue';
+  import VueNavigationProgress from '@components/VueNavigationProgress/VueNavigationProgress.vue';
+  import VueSidebar from '@components/VueSidebar/VueSidebar.vue';
+  import VueSidebarGroup from '@components/VueSidebar/VueSidebarGroup/VueSidebarGroup.vue';
+  import VueSidebarGroupItem from '@components/VueSidebar/VueSidebarGroupItem/VueSidebarGroupItem.vue';
+  import VueIconCode from '@components/icons/VueIconCode/VueIconCode.vue';
+  import VueIconBook from '@components/icons/VueIconBook/VueIconBook.vue';
+  import VueIconHashtag from '@components/icons/VueIconHashtag/VueIconHashtag.vue';
+  import VueIconGithub from '@components/icons/VueIconGithub/VueIconGithub.vue';
+  import VueIconTwitterSquare from '@components/icons/VueIconTwitterSquare/VueIconTwitterSquare.vue';
+  import VueSelect from '@components/VueSelect/VueSelect.vue';
+  import VueIconPuzzlePiece from '@components/icons/VueIconPuzzlePiece/VueIconPuzzlePiece.vue';
+  import VueButton from '@components/VueButton/VueButton.vue';
+  import VueModal from '@components/VueModal/VueModal.vue';
+  import LoginForm from '@shared/modules/auth/LoginForm/LoginForm.vue';
+  import { addNotification } from '@components/VueNotificationStack/utils';
 
-export default {
-  name: 'App',
-  components: {
-    VueNavBar,
-    LoginForm,
-    VueModal,
-    VueButton,
-    VueIconPuzzlePiece,
-    VueSelect,
-    VueIconTwitterSquare,
-    VueIconGithub,
-    VueIconHashtag,
-    VueIconBook,
-    VueIconCode,
-    VueSidebarGroupItem,
-    VueSidebarGroup,
-    VueSidebar,
-    VueNavigationProgress,
-    VueCookieConsent,
-    PuddorNavBar,
-    VueGrid,
-    VueGridItem,
-    VueFooter,
-    VueNotificationStack,
-  },
-  data(): any {
-    return {
-      isNavigating: true,
-      languages: [{ label: '中文', value: 'zh-cn' }, { label: 'English', value: 'en' }],
-      showLoginModal: false,
-      isLoginPending: false,
-    };
-  },
-  computed: {
-    ...mapGetters('app', ['cookieConsentVersion', 'getLocale']),
-    ...mapGetters('auth', ['isAuthenticated']),
-  },
-  methods: {
-    ...mapActions('app', ['changeLocale', 'setCookieConsentVersion']),
-    ...mapActions('auth', ['createToken', 'revokeToken']),
-    localeSwitch(locale: string) {
-      loadLocaleAsync(locale).catch((error: Error) => console.log(error)); // tslint:disable-line
+  export default {
+    name: 'App',
+    components: {
+      VueNavBar,
+      LoginForm,
+      VueModal,
+      VueButton,
+      VueIconPuzzlePiece,
+      VueSelect,
+      VueIconTwitterSquare,
+      VueIconGithub,
+      VueIconHashtag,
+      VueIconBook,
+      VueIconCode,
+      VueSidebarGroupItem,
+      VueSidebarGroup,
+      VueSidebar,
+      VueNavigationProgress,
+      VueCookieConsent,
+      PuddorNavBar,
+      VueGrid,
+      VueGridItem,
+      VueFooter,
+      VueNotificationStack,
+    },
+    data(): any {
+      return {
+        isNavigating: true,
+        languages: [{ label: '中文', value: 'zh-cn' }, { label: 'English', value: 'en' }],
+        showLoginModal: false,
+        isLoginPending: false,
+      };
+    },
+    computed: {
+      ...mapGetters('app', ['cookieConsentVersion', 'getLocale']),
+      ...mapGetters('auth', ['isAuthenticated']),
+    },
+    methods: {
+      ...mapActions('app', ['changeLocale', 'setCookieConsentVersion']),
+      ...mapActions('auth', ['createToken', 'revokeToken']),
+      localeSwitch(locale: string) {
+        loadLocaleAsync(locale).catch((error: Error) => console.log(error)); // tslint:disable-line
 
-      this.changeLocale(locale);
-    },
-    toHome() {
-      this.$router.push('/');
-    },
-    toForum() {
-      this.$router.push('/forum/home');
-    },
-    toEmall() {
-      this.$router.push('/emall/home');
-    },
-    toDownLoad() {
-      this.$router.push('/document/home');
-    },
-    initProgressBar() {
-      this.$router.beforeEach((to: any, from: any, next: any) => {
-        this.isNavigating = true;
-        next();
-      });
-      this.$router.afterEach(() => {
-        this.isNavigating = false;
-      });
-    },
-    async onLoginSubmit(formData: any) {
-      this.isLoginPending = true;
+        this.changeLocale(locale);
+      },
+      toHome() {
+        this.$router.push('/');
+      },
+      toForum() {
+        this.$router.push('/forum/home');
+      },
+      toEmall() {
+        this.$router.push('/emall/home');
+      },
+      toDownLoad() {
+        this.$router.push('/document/home');
+      },
+      initProgressBar() {
+        this.$router.beforeEach((to: any, from: any, next: any) => {
+          this.isNavigating = true;
+          next();
+        });
+        this.$router.afterEach(() => {
+          this.isNavigating = false;
+        });
+      },
+      async onLoginSubmit(formData: any) {
+        this.isLoginPending = true;
 
-      try {
-        await this.createToken(formData);
+        try {
+          await this.createToken(formData);
 
-        this.$router.push({ name: 'dashboard' });
-      } catch (e) {
-        addNotification({ title: 'Error during login', text: 'Please try again!' });
-      }
+          this.$router.push({ name: 'dashboard' });
+        } catch (e) {
+          addNotification({ title: 'Error during login', text: 'Please try again!' });
+        }
 
-      this.isLoginPending = false;
-      this.showLoginModal = false;
+        this.isLoginPending = false;
+        this.showLoginModal = false;
+      },
+      async onLogout() {
+        this.isLoginPending = true;
+
+        await this.revokeToken();
+
+        this.$router.push('/');
+
+        this.isLoginPending = false;
+        this.showLoginModal = false;
+      },
     },
-    async onLogout() {
-      this.isLoginPending = true;
-
-      await this.revokeToken();
-
-      this.$router.push('/');
-
-      this.isLoginPending = false;
-      this.showLoginModal = false;
+    created() {
+      this.initProgressBar();
     },
-  },
-  created() {
-    this.initProgressBar();
-  },
-};
+  };
 </script>
 
 <style lang="scss" module>
-@import '~@/app/shared/design-system';
-@import '~@/app/shared/designSystem/reset';
-@import '~@/app/shared/designSystem/typo';
+    @import '~@/app/shared/design-system';
+    @import '~@/app/shared/designSystem/reset';
+    @import '~@/app/shared/designSystem/typo';
 
-.app {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  -webkit-font-smoothing: antialiased; /*chrome、safari*/
-  -moz-osx-font-smoothing: grayscale; /*firefox*/
-  font-family: '仿宋';
-  vertical-align: bottom;
-}
+    .app {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        -webkit-font-smoothing: antialiased; /*chrome、safari*/
+        -moz-osx-font-smoothing: grayscale; /*firefox*/
+        font-family: '仿宋';
+        vertical-align: bottom;
+    }
 
-.content {
-  flex: 1;
-}
+    .content {
+        flex: 1;
+    }
 
-.logo {
-  position: relative;
-  top: $space-4;
-  width: $space-24;
-  height: $space-24;
-}
+    .logo {
+        position: relative;
+        top: $space-4;
+        width: $space-24;
+        height: $space-24;
+    }
 </style>
--- src/app/app/App/componentsPuddor/puddorNavBar.vue
+++ src/app/app/App/componentsPuddor/puddorNavBar.vue
@@ -1,19 +1,21 @@
 <template>
     <div class="top-bar-nav-content">
         <div class="top-bar-nav-wraper">
-            <div class="side-bar">
-                <vue-sidebar style="background-color: white">
-                    <vue-sidebar-group title="导航">
-                        <vue-sidebar-group-item to="/">
-                            一键找乐手
-                        </vue-sidebar-group-item>
-                    </vue-sidebar-group>
+            <div class="side-bar-wraper">
+                <div class="side-bar">
+                    <vue-sidebar style="background-color: white">
+                        <vue-sidebar-group title="导航">
+                            <vue-sidebar-group-item to="/">
+                                一键找乐手
+                            </vue-sidebar-group-item>
+                        </vue-sidebar-group>
 
-                </vue-sidebar>
-            </div>
-            <div class="top-bar-nav-left">
-                <img class="nav-img-logo" src="logo.png" alt=""/>
-                <span class="nav-img-text">Puddor</span>
+                    </vue-sidebar>
+                </div>
+                <div class="top-bar-nav-left">
+                    <img class="nav-img-logo" src="logo.png" alt=""/>
+                    <span class="nav-img-text">Puddor</span>
+                </div>
             </div>
             <div class="top-bar-nav-right">
                 <div>
@@ -55,14 +57,14 @@
     components: {
       VueSidebarGroupItem,
       VueSidebarGroup,
-      VueSidebar
-    }
+      VueSidebar,
+    },
   };
 </script>
 
 <style lang="scss" scoped>
-
-    @media screen and (min-width: 960px) {
+    /*窗口 > 1400*/
+    @media screen and (min-width: 1400px) {
         .top-bar-nav-content {
             position: fixed;
             top: 0;
@@ -72,13 +74,13 @@
             display: flex;
             justify-content: space-between;
             align-items: center;
-            z-index: 999999;
+            z-index: 99;
         }
         .top-bar-nav-wraper {
             margin: 0 auto;
             min-width: 1400px;
             display: flex;
-            justify-content: space-around;
+            justify-content: space-between;
             align-items: center;
         }
         .nav-img-logo {
@@ -95,6 +97,7 @@
         .top-bar-nav-left {
             display: flex;
             align-items: center;
+            margin-left: 50px;
         }
         .top-bar-nav-right {
             color: #0d9660;
@@ -128,14 +131,123 @@
         }
     }
 
-    /*窗口< 960px*/
+    /*窗口 960 ~ 1400*/
+    @media screen and (max-width: 1400px) {
+        .top-bar-nav-content {
+            position: fixed;
+            top: 0;
+            width: 100%;
+            background-color: #0c1014;
+            padding: 1.2em 0;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            z-index: 99;
+        }
+        .top-bar-nav-wraper {
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+        .nav-img-logo {
+            width: 22px;
+            height: 31px;
+        }
+        .nav-img-text {
+            margin-left: 9px;
+            height: 14px;
+            line-height: 14px;
+            color: rgba(255, 255, 255, 1);
+            font-size: 22px;
+        }
+        .top-bar-nav-left {
+            display: flex;
+            align-items: center;
+            margin-left: 50px;
+        }
+        .top-bar-nav-right {
+            color: #0d9660;
+        }
+        .top-bar-nav-item {
+            margin-right: 45px;
+            height: 28px;
+            font-size: 20px;
+            font-weight: 400;
+            color: rgba(255, 255, 255, 1);
+            line-height: 28px;
+            -webkit-font-smoothing: antialiased; /*chrome、safari*/
+            -moz-osx-font-smoothing: grayscale; /*firefox*/
+            > span :hover {
+                cursor: pointer;
+            }
+        }
+        .top-nav-wraper {
+            display: flex;
+            align-items: center;
+        }
+        .top-nav-item :hover {
+            cursor: pointer;
+        }
+        .top-nav-item {
+            line-height: 28px;
+            height: 28px;
+            margin-right: 50px;
+            vertical-align: bottom;
+            color: #ffffff;
+        }
+
+    }
+
+    /*窗口 768 ~ 960*/
     @media screen and (max-width: 960px) {
         .top-bar-nav-wraper {
             display: none;
         }
+        .top-bar-nav-content {
+            position: fixed;
+            top: 0;
+            width: 100%;
+            background-color: #0c1014;
+            padding: 1.2em 0;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            z-index: 99;
+        }
+        .top-bar-nav-wraper {
+            margin: 0 auto;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+        }
+        /deep/ ._2YA7icQELk7ZHr3bFAXn4y {
+            background-color: white;
+        }
+        .side-bar {
+            background-color: #0d9660;
+        }
+        .nav-img-logo {
+            width: 22px;
+            height: 31px;
+        }
+        .nav-img-text {
+            margin-left: 9px;
+            height: 14px;
+            line-height: 14px;
+            color: rgba(255, 255, 255, 1);
+            font-size: 22px;
+        }
+        .top-bar-nav-left {
+            display: flex;
+            align-items: center;
+        }
+        .top-bar-nav-right {
+            display: none;
+        }
     }
 
-    /*窗口< 768px*/
+    /*窗口 < 768px*/
     @media screen and (max-width: 768px) {
         .top-bar-nav-content {
             position: fixed;
@@ -146,7 +258,7 @@
             display: flex;
             justify-content: space-between;
             align-items: center;
-            z-index: 999999;
+            z-index: 99;
         }
         .top-bar-nav-wraper {
             margin: 0 auto;
@@ -154,7 +266,7 @@
             justify-content: space-around;
             align-items: center;
         }
-        /deep/._2YA7icQELk7ZHr3bFAXn4y {
+        /deep/ ._2YA7icQELk7ZHr3bFAXn4y {
             background-color: white;
         }
         .side-bar {
